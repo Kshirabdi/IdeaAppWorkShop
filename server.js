48d9698267bebbd36c877fdb9d152c3a849d4d42
Kshirabdi <swainkshirabditanaya@gmail.com>
2023-07-02 01:08:02 +0530

    modify

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express= require('express');
 
 const app = express();
 
-app.listen(3333, ()=>{
-    console.log("serevr started");
-})
+app.listen(3333, ()=>
+console.log("Server started")
+
+)
